4cc3dcbe3fff061eaedecce80f8cc659bc746190
Rohit Khoja <32352533+rohitkhoja@users.noreply.github.com>
2025-02-26 22:03:41 -0700

    Time: 29 ms (72.70%) | Memory: 51.6 MB (59.56%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,57 +1,24 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Map<Integer,Integer> elements = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(elements.containsKey(nums[i])) elements.put(nums[i],elements.get(nums[i])+1);
-            else elements.put(nums[i],1);
-        }
-        System.out.println(elements);
-        Set<List<Integer>> check = new HashSet<>();
+        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
-        for(int i=0;i<nums.length;i++)
-        for(int j=i+1;j<nums.length;j++){
-            if(nums[i]==0 && nums[j]==0 && elements.get(nums[i]) > 2){
-                    List<Integer> temp = 
-                    new ArrayList<>(List.of(nums[i],nums[j],-(nums[i]+nums[j])));
-                Collections.sort(temp);
-                if(!check.contains(temp)){
-                    check.add(temp);
-                    result.add(temp);
-                }
+        for(int i=0;i<nums.length-1;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            else {
+                int j=i+1;
+                int k=nums.length-1;
+                while(j<k){
+                    int t = nums[i]+nums[j]+nums[k];
+                    if(t>0) k--;
+                    else if(t<0) j++;
+                    else{
+                        result.add(new ArrayList<>(List.of(nums[i],nums[j],nums[k])));
+                        j++;
+                        while(nums[j-1]==nums[j] && j<k) j++;
+                    }
+
+                } 
             }
-            else if((nums[i]+nums[j] == -(nums[i])) && elements.get(nums[i]) >=2 && !(nums[i]==nums[j])){
-                List<Integer> temp = 
-                    new ArrayList<>(List.of(nums[i],nums[j],-(nums[i]+nums[j])));
-                Collections.sort(temp);
-                if(!check.contains(temp)){
-                    check.add(temp);
-                    result.add(temp);
-                }
-            }
-            else if((nums[i]+nums[j] == -(nums[j]))&& elements.get(nums[j]) >=2 && !(nums[i]==nums[j])){
-                List<Integer> temp = 
-                    new ArrayList<>(List.of(nums[i],nums[j],-(nums[i]+nums[j])));
-                Collections.sort(temp);
-                if(!check.contains(temp)){
-                    check.add(temp);
-                    result.add(temp);
-                }
-            }
-               
-            else if(elements.containsKey(-(nums[i]+nums[j])) 
-                    && !(nums[i]==nums[j]) && !(nums[i]+nums[j] == -(nums[j])) 
-                            && !(nums[i]+nums[j] == -(nums[i]))){
-                List<Integer> temp = 
-                    new ArrayList<>(List.of(nums[i],nums[j],-(nums[i]+nums[j])));
-                Collections.sort(temp);
-                if(!check.contains(temp)){
-                    check.add(temp);
-                    result.add(temp);
-                }
-            }
-            else continue;
-             
-           
         }
         return result;
     }
