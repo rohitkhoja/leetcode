e2911cfb8f5f7feca4bffb0b52fa5c5e41d8d71c
Rohit Khoja <32352533+rohitkhoja@users.noreply.github.com>
2025-09-11 21:06:07 -0400

    Time: 7 ms (21.97%) | Memory: 45.4 MB (6.17%) - LeetSync

--- 71-simplify-path/simplify-path.java
+++ 71-simplify-path/simplify-path.java
@@ -1,67 +1,24 @@
 class Solution {
     public String simplifyPath(String path) {
-        int l = path.length();
+        Deque<String> stack = new ArrayDeque<>();
+        String[] dirs = path.split("/");
         String result = "";
-        int countDot = 0;
-        int countSlash = 0;
-        for(int i=l-1;i>=0;){
-            if(path.charAt(i)=='/'){
-                if(i!=l-1) result+='/';
-                while(i>=0 && path.charAt(i)=='/') i--;
-                
-            }
-            else if(path.charAt(i)=='.' && i+1<=l-1 && path.charAt(i+1)!='/'){
-                while(i>=0 && path.charAt(i)=='.'){
-                        i--;
-                        result+='.';
-                    }
-            }
-            else if(path.charAt(i)=='.'){
-                while(i>=0 && path.charAt(i)=='.'){
-                    i--;
-                    countDot++;
-                }
-                System.out.println(countDot+" "+i);
-
-                if(countDot>2 || path.charAt(i)!='/'){
-                    while(countDot>0){
-                       result+='.';
-                       countDot--;
-                    }
-                } 
-                else if(countDot==2){
-                    countSlash = 2;
-                    while(i>=0 && countSlash>0){
-                        if(path.charAt(i)=='/') {
-                            while(i>=0 && path.charAt(i)=='/') i--;
-                            countSlash--;
-                        }
-                        else if(path.charAt(i)=='.'){
-                            int c = 0;
-                            while(i>=0 && path.charAt(i)=='.'){
-                                i--;
-                                c++;
-                            } 
-                            if(c==2) countSlash+=2;
-                            else if(c==1 && i+2<l-1 && path.charAt(i+2)=='/') countSlash+=1;
-                        }
-                        else i--;
-                    }
-                    countDot=0;
-                }
-                else {
-                    while(i>=0 &&  path.charAt(i)=='/') i--;
-                    countDot=0;
-                }
-
+       
+        for(String dir: dirs){
+            if(dir.equals("") || dir.equals(".")) continue;
+            else if(dir.equals("..")) {
+                if(stack.isEmpty()) continue;
+                else stack.pop();
             }
             else {
-                result+=path.charAt(i);
-                i--;
+              stack.push(dir);
             }
         }
-        String result1 = new StringBuilder(result).reverse().toString();
+        while(!stack.isEmpty()){
+            result = "/"+stack.pop()+result;
+
+        }
         if(result.length()==0) return "/";
-        else return result1;
+        return result;
     }
 }
