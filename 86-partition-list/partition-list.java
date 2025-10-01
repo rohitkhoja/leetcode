25917ea849c80dd33144a28c692cc0c0ca6a483c
Rohit Khoja <32352533+rohitkhoja@users.noreply.github.com>
2025-09-30 21:19:38 -0400

    Time: 0 ms (100.00%) | Memory: 42.3 MB (31.01%) - LeetSync

--- 86-partition-list/partition-list.java
+++ 86-partition-list/partition-list.java
@@ -10,37 +10,43 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode tempFirst = head;
-        ListNode tempMid = null;
-        ListNode tempLast = head;
-        while(tempLast!=null){
-            while(tempFirst!=null && tempFirst.val<x) tempFirst = tempFirst.next;
-            if(tempFirst==null) return head;
-            tempLast = tempFirst.next;
-            while(tempLast!=null) {
-                if(tempLast.val>=x) tempMid = tempLast;
-                else break;
-                tempLast = tempLast.next;
+        if(head==null) return head;
+        ListNode left = null;
+        ListNode right = null;
+        ListNode leftHead = null;
+        ListNode rightHead = null;
+        while(head!=null){
+            if(head.val<x){
+                if(left==null){
+                    left = head;
+                    leftHead = head;
+                }
+                else {
+                    left.next = head;
+                    left = head;
+                }
             }
-            if(tempLast==null) return head;
-            if(tempMid!=null) {
-                int val = tempMid.val;
-                tempMid.val = tempLast.val;
-                tempLast.val = val;
-                tempLast = tempMid;
-                tempMid = null;
+            else {
+                if(right==null){
+                    right = head;
+                    rightHead = head;
+                }
+                else {
+                    right.next = head;
+                    right = head;
+                }
             }
-            else{
-                int val = tempFirst.val;
-                tempFirst.val = tempLast.val;
-                tempLast.val = val;
-                tempLast = tempLast.next;
-                tempFirst = tempFirst.next;
-            }
-            
+            head = head.next;
+        }   
+        if(right==null) return leftHead;
+        if(left==null) return rightHead;
+        right.next = null;
+        left.next = rightHead;
+        return leftHead;
+        
+        
 
-        }
-        return head;
+        
         
     }
 }
