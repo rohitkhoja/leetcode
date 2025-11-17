e82c2e23865f7fb94877a9891058d2bb9ff51cd0
Rohit Khoja <32352533+rohitkhoja@users.noreply.github.com>
2025-11-17 15:22:15 -0500

    Time: 91 ms (10.46%) | Memory: 119.6 MB (22.80%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -29,11 +29,15 @@ class Solution {
             Pair p = pq.poll();
             result.add(new ArrayList<>(List.of(nums1[p.i], nums2[p.j])));
 
-            if(p.i+1<n1 && !set.contains((p.i+1)+" "+p.j)) pq.add(new Pair(nums1[p.i+1]+nums2[p.j],p.i+1,p.j));
-            set.add((p.i+1)+" "+p.j);
+            if(p.i+1<n1 && !set.contains((p.i+1)+" "+p.j)) {
+                pq.add(new Pair(nums1[p.i+1]+nums2[p.j],p.i+1,p.j));
+                set.add((p.i+1)+" "+p.j);
+            }
             //System.out.println((p.i+1)+""+p.j);
-            if(p.j+1<n2&& !set.contains(p.i+" "+(p.j+1))) pq.add(new Pair(nums1[p.i]+nums2[p.j+1],p.i,p.j+1));
-            set.add(p.i+" "+(p.j+1));
+            if(p.j+1<n2&& !set.contains(p.i+" "+(p.j+1))){
+                pq.add(new Pair(nums1[p.i]+nums2[p.j+1],p.i,p.j+1));
+                set.add(p.i+" "+(p.j+1));
+            } 
             //System.out.println(p.i+""+(p.j+1));
             k--;
             
