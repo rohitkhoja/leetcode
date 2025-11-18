7037595907eb153e97e14e65bfc911aedd0ee774
Rohit Khoja <32352533+rohitkhoja@users.noreply.github.com>
2025-11-17 20:04:05 -0500

    Time: 32 ms (27.27%) | Memory: 99.2 MB (10.49%) - LeetSync

--- 3387-minimum-operations-to-make-median-of-array-equal-to-k/minimum-operations-to-make-median-of-array-equal-to-k.java
+++ 3387-minimum-operations-to-make-median-of-array-equal-to-k/minimum-operations-to-make-median-of-array-equal-to-k.java
@@ -4,33 +4,39 @@ class Solution {
         int n = nums.length;
         //if(n==2) return Math.abs(k-nums[1]);
         int mid = n/2;
-        int index = binarySearch(nums, k, 0, n-1, mid);
-        //System.out.println(index+" index");
-        
-        if(index==mid) return Math.abs(k-nums[mid]);
         long sum=0;
-        int i = Math.max(index,mid);
-        int j = Math.min(index, mid);
-        while(j<=i){
-            sum += Math.abs(k-nums[j]);
-            j++;
+        
+        if(nums[mid]>k){
+            
+            while(mid>=0 && nums[mid]>k){
+                sum += Math.abs(nums[mid]-k);
+                mid--;
+            }
+            return sum;
         }
-        return sum;
+        else {
+            while(mid<n && nums[mid]<k){
+                sum += Math.abs(nums[mid]-k);
+                mid++;
+            }
+            return sum;
+        }
+        // ;
 
     }
 
-    public int binarySearch(int[] nums, int target, int start, int end, int m){
-        if(start>end) {
-            if(end<0) return start;
-            // System.out.println(start+" start");
-            // System.out.println(end+" end");
-            if(start>=nums.length) return end;
-            if(nums[start]>target && start<=m) return start;
-            else return end;
-        }
+    // public int binarySearch(int[] nums, int target, int start, int end, int m){
+    //     if(start>end) {
+    //         if(end<0) return start;
+    //         // System.out.println(start+" start");
+    //         // System.out.println(end+" end");
+    //         if(start>=nums.length) return end;
+    //         if(nums[start]>target && start<=m) return start;
+    //         else return end;
+    //     }
 
-        int mid = (start+end)/2;
-        if(nums[mid]>target) return binarySearch(nums, target, start, mid-1, m);
-        else return binarySearch(nums, target, mid+1, end, m);
-    }
+    //     int mid = (start+end)/2;
+    //     if(nums[mid]>target) return binarySearch(nums, target, start, mid-1, m);
+    //     else return binarySearch(nums, target, mid+1, end, m);
+    // }
 }
