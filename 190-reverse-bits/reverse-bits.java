88926981be72a53103eba6f5b64924606452de03
Rohit Khoja <32352533+rohitkhoja@users.noreply.github.com>
2025-11-18 13:47:58 -0500

    Time: 0 ms (100.00%) | Memory: 42.4 MB (12.88%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,13 +1,14 @@
 class Solution {
     public int reverseBits(int n) {
         int result = 0;
-
-        for(int i=0;i<32;i++){
+        int i=32;
+        while(i>0){
             result <<= 1;
 
             result |= (n&1);
 
-            n >>>= 1;
+            n >>>= 1;;
+            i--;
 
         }
 
