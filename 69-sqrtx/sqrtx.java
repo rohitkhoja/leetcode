d0b2081a6252b30184612c41973dc185d5e45150
Rohit Khoja <32352533+rohitkhoja@users.noreply.github.com>
2025-11-21 00:39:36 -0500

    Time: 50 ms (5.17%) | Memory: 46.2 MB (9.46%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        return (int) binarySearch(0,x,x);
+    }
+
+    public long binarySearch(long start, long end, long target){
+        if(start>end){
+            return end;
+        }
+
+        long mid = (start+end)/2;
+        System.out.println(mid);
+        if(target== (long) mid*mid) return mid;
+        if(target > (long) (mid*mid) && target < (long) (mid+1)*(mid+1)) return mid;
+        if(target > (long) (mid*mid)) return binarySearch(mid+1, end, target);
+        else return binarySearch(start, mid-1, target);
+
+    }
+}
