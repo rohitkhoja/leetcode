d63a9a4b9fd2e119248a9ffc7e0229f1fa602a1b
Rohit Khoja <32352533+rohitkhoja@users.noreply.github.com>
2025-11-21 20:51:24 -0500

    Time: 24 ms (71.29%) | Memory: 46.9 MB (6.77%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.java
+++ 149-max-points-on-a-line/max-points-on-a-line.java
@@ -2,10 +2,10 @@ class Solution {
     public int maxPoints(int[][] points) {
         Map<Double, Integer> map;
         int[] result = new int[points.length];
-
+        if(points.length<3) return points.length;
         for(int i=0;i<points.length;i++){
             map = new HashMap<>();
-            for(int j=0; j<points.length;j++){
+            for(int j=i+1; j<points.length;j++){
                 double slope = 0;
                 if(i==j)
                     continue;
@@ -20,19 +20,12 @@ class Solution {
 
                 map.put(slope, map.getOrDefault(slope, 0)+1);
                 result[i] = Math.max(map.get(slope), result[i]);
-                //System.out.println("slope "+slope+" i and j"+i+j);
-                // Set<int[]> set = map.getOrDefault(slope, new HashSet<>());
-                // set.add(new int[]{points[i][0], points[i][1]});
-                // set.add(new int[]{points[j][0], points[j][1]});
-                // System.out.println(set);
-                //if(map.get(slope)>result) System.out.println("slope "+slope+" i and j"+i+j); 
-                //result = Math.max(result, map.get(slope));
             }
         }
         int rs = 0;
         for(int i=0;i<result.length;i++)
             rs = Math.max(rs, result[i]);
-        //System.out.println(map);
+
         return rs+1;
     }
 }
