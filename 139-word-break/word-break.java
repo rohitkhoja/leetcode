974f4c109cd897f22b1c0f6d2af6f3ce6ab6b5df
Rohit Khoja <32352533+rohitkhoja@users.noreply.github.com>
2025-11-25 02:55:59 -0500

    Time: 4 ms (67.65%) | Memory: 43.3 MB (68.07%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -6,7 +6,6 @@ class Solution {
         if(n==0) return true;
         boolean[] isPossible = new boolean[n+1];
         isPossible[n] = true;
-        StringBuilder sb = new StringBuilder();
 
         for(int i=n-1;i>=0;i--){
             for( String word: wordDict){
