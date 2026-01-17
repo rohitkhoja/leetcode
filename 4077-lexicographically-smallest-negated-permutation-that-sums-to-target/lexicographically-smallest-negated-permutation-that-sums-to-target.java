9303231135784635c9b282d4ef88dbef8d99897c
Rohit Khoja <32352533+rohitkhoja@users.noreply.github.com>
2026-01-17 09:36:43 -0700

    Time: 42 ms (9.83%) | Memory: 221.1 MB (5.01%) - LeetSync

--- 4077-lexicographically-smallest-negated-permutation-that-sums-to-target/lexicographically-smallest-negated-permutation-that-sums-to-target.java
+++ 4077-lexicographically-smallest-negated-permutation-that-sums-to-target/lexicographically-smallest-negated-permutation-that-sums-to-target.java
@@ -1,31 +1,42 @@
 class Solution {
     public int[] lexSmallestNegatedPerm(int n, long target) {
-
-        List<Integer> ps = new ArrayList<>();
-        Set<Integer> ng = new HashSet<>();
-
-        long sum = ((long) n*(n+1))/2;
-        if(Math.abs(target)>sum) return new int[0];
-        if(Math.abs(sum-target)%2!=0) return new int[0];
         
-        sum = (sum-target)/2;
-        int temp = n;
-        while(sum>0 && temp>=1){
-           if(temp<=sum){
-               sum -= temp;
-               ng.add(temp);
-           }
-           temp--;
+        long maxSum = (long) n * (n+1) / 2;
+        if(Math.abs(target) > maxSum) return new int[0];
+
+        if((maxSum - target)%2!=0) return new int[0];
+
+        long sum = (maxSum - target) / 2;
+        Set<Integer> set = new HashSet<>();
+        List<Integer> result = new ArrayList<>();
+        for(int i=n;i>0;i--){
+            if(sum==0) break;
+            if(i>sum){
+               set.add((int) sum);
+               result.add((int) -sum);
+               sum = 0;
+               break;
+            }
+
+            else{
+               set.add(i);
+               result.add(-i);
+               sum -= i;
+            }
         }
 
-        int[] result = new int[n];
-        for(int i=0;i<n;i++){
-            if(ng.contains(i+1))
-                result[i] = -(i+1);
-            else result[i] = i+1;
-        }
+        if(sum!=0) return new int[0];
         
-        Arrays.sort(result);
-        return result;
+        for(int i=1;i<=n;i++){
+            if(!set.contains(i)){
+                result.add(i);
+            }
+        }
+
+        int[] rs = new int[n];
+        for(int i=0;i<n;i++)
+            rs[i] = result.get(i);
+
+        return rs;    
     }
 }
