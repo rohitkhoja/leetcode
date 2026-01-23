95f114b8a4d51076b392e8b37a6e60b9dbce17a9
Rohit Khoja <32352533+rohitkhoja@users.noreply.github.com>
2026-01-23 11:53:58 -0700

    Time: 7 ms (75.77%) | Memory: 49.5 MB (79.98%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,19 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<Integer,List<String>> map = new HashMap<>();
-        int n = strs.length;
-        for(int i=0;i<n;i++){
-            char[] chars = strs[i].toCharArray();
-            Arrays.sort(chars);
-            String s = new String(chars);
-            int hash = s.hashCode();
-            map.computeIfAbsent(hash, k-> new ArrayList<>()).add(strs[i]);
+        Map<Integer, List<String>> map = new HashMap<>();
+
+        for(String str: strs){
+            char[] chArray = str.toCharArray();
+            Arrays.sort(chArray);
+            
+            String s = new String(chArray);
+            int hashCode = s.hashCode();
+            map.computeIfAbsent(hashCode, k-> new ArrayList<>()).add(str);
         }
         return new ArrayList<>(map.values());
     }
 }
-
-
-// first sort, get hash values
-// 1,2,1,3,4,5
-// hash value as key and keep on adding them to mpa and return them 
