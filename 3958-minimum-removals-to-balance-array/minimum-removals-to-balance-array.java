c2d358ddc77b8bbc7e1909a0b9f6c93421391131
Rohit Khoja <32352533+rohitkhoja@users.noreply.github.com>
2026-02-05 18:28:38 -0700

    Time: 24 ms (93.17%) | Memory: 86.3 MB (36.95%) - LeetSync

--- 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.java
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.java
@@ -3,37 +3,13 @@ class Solution {
 
         Arrays.sort(nums);
         int n = nums.length;
-        int result = n+1;
+
+        int l = 0;
+        int result = n+1;;
         for(int i=0;i<n;i++){
-            long target = (long) nums[i]*k;
-            int start = 0;
-            int end = n-1;
-
-            while(start<end){
-                int mid = (start+end)/2;
-                if(nums[mid]>target) {
-                    end = mid-1;
-                }
-                else if(nums[mid]<target){
-                    start = mid+1;
-                }
-                else{
-                    while(mid+1<n && nums[mid]==nums[mid+1]) mid++;
-                    start=mid;
-                    break;
-                }
-            }
-            if(start==n) result = Math.min(result, n-i);
-            else if(nums[start]>target) result = Math.min(result, n-(start-i));
-            else if(start+1<n && nums[start+1]<=target) result = Math.min(result, n-(start+2-i));
-            else result = Math.min(result, n-(start+1-i));
-
+            while(nums[i] > (long) nums[l]*k) l++;
+            result = Math.min(result, n - (i-l+1));
         }
-
         return result;
-
-        
     }
-
-
 }
