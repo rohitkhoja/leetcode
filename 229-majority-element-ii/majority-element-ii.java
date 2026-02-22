e2b2d346aa9e34b3a785911181c6c5d16bbeab77
Rohit Khoja <32352533+rohitkhoja@users.noreply.github.com>
2026-02-21 18:26:18 -0700

    Time: 3 ms (75.35%) | Memory: 52.5 MB (63.56%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,19 +1,42 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        int num1=Integer.MAX_VALUE;
+        int num2=Integer.MAX_VALUE;
+        int count1=0;
+        int count2=0;
 
-        Arrays.sort(nums);
         List<Integer> list = new ArrayList<>();
-        int n = nums.length;
-        for(int i=0;i<nums.length;i++){
-            int count = 1;
-            while(i+1<n && nums[i]==nums[i+1]) {
-                count++;
-                i++;
+        for(int num:nums){
+            //if(count1==0) num1 = num;
+            if(num==num1) count1++;
+            else if(num==num2) count2++;
+            else if(count1==0) {
+                num1 = num;
+                count1++;
             }
-            //i--;
-            if(count > n/3) list.add(nums[i]);
+            else if(count2==0) {
+                num2=num;
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
         }
+        count1=0;
+        count2=0;
+        for(int num:nums){
+            
+            if(num==num1) count1++;
+            if(num==num2) count2++;
+            
+        }
+        int n = nums.length;
+
+        if(count1>n/3) list.add(num1);
+        if(count2>n/3) list.add(num2);
 
         return list;
+        
     }
 }
