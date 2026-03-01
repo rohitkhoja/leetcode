34f6f98f5a7210468c60a67d46d944541e08eddc
Rohit Khoja <32352533+rohitkhoja@users.noreply.github.com>
2026-03-01 10:40:42 -0700

    Time: 15 ms (62.78%) | Memory: 51.4 MB (87.83%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -6,8 +6,8 @@ class Solution {
         int result = 0;
         int j=0;
         int n = g.length;
-        for(int i=0; i<s.length;i++){
-            if(j<n && s[i]>=g[j]){
+        for(int i=0; i<s.length && j<n;i++){
+            if(s[i]>=g[j]){
                 result++;
                 j++;
             }
